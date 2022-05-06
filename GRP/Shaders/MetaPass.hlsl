71006ebd1e153ee1222c16d08906727db6081d67
aGEM <gem0816qiqiqi@163.com>
2022-05-06 10:06:50 +0800

    漏传文件

---
+++ GRP/Shaders/MetaPass.hlsl
@@ -0,0 +1,52 @@
+#ifndef GRP_META_PASS_INCLUDED
+#define GRP_META_PASS_INCLUDED
+#include "../ShaderLibrary/Surface.hlsl"
+#include "../ShaderLibrary/Shadows.hlsl"
+#include "../ShaderLibrary/Light.hlsl"
+#include "../ShaderLibrary/BRDF.hlsl"
+
+bool4 unity_MetaFragmentControl;
+float unity_OneOverOutputBoost;
+float unity_MaxOutputValue;
+
+struct Attributes {
+    float3 positionOS : POSITION;
+    float2 baseUV : TEXCOORD0;
+    float2 lightMapUV : TEXCOORD1;
+};
+
+struct Varyings {
+    float4 positionCS : SV_POSITION;
+    float2 baseUV : VAR_BASE_UV;
+};
+
+Varyings MetaPassVertex (Attributes input) {
+    Varyings output;
+    input.positionOS.xy =
+        input.lightMapUV * unity_LightmapST.xy + unity_LightmapST.zw;
+    output.positionCS = TransformWorldToHClip(input.positionOS);
+    input.positionOS.z > 0.0 ? FLT_MIN : 0.0;
+    output.baseUV = TransformBaseUV(input.baseUV);
+    return output;
+}
+
+float4 MetaPassFragment(Varyings input): SV_TARGET 
+{
+    float4 base = GetBase(input.baseUV);
+    Surface surface;
+    ZERO_INITIALIZE(Surface, surface);
+    surface.color = base.rgb;
+    surface.metallic = GetMetallic(input.baseUV);
+    surface.smoothness = GetSmoothness(input.baseUV);
+    BRDF brdf = GetBRDF(surface);
+    float4 meta = 0.0;
+    if (unity_MetaFragmentControl.x)
+    {
+        meta = float4(brdf.diffuse,1.0);
+        //The idea behind this is that highly specular but rough materials also pass along some indirect light.
+        meta.rgb += brdf.specular * brdf.roughness * 0.5;
+        meta.rgb = min(PositivePow(meta.rgb, unity_OneOverOutputBoost), unity_MaxOutputValue);
+    }
+    return meta;
+}
+#endif
